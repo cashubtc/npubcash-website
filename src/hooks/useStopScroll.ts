654e447b0b4e5140b0f55e8aae7eb7058d52ac97
Egge <egge24@protonmail.com>
2024-02-26 10:24:00 +0100

    fixed modal centering

--- src/hooks/useStopScroll.ts
+++ src/hooks/useStopScroll.ts
@@ -2,7 +2,6 @@ import { useEffect } from "react";
 
 export const useStopScroll = () => {
   useEffect(() => {
-    window.scrollTo(0, 0);
     document.body.style.overflow = "hidden";
     return () => {
       document.body.style.overflow = "unset";
