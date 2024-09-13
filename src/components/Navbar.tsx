34e46aadc537828e68bba44b7e79b6ba511439a3
Egge <egge@getcurrent.io>
2024-09-13 07:33:05 +0200

    updated navbar links

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,6 +7,7 @@ const Navbar = () => {
   const { sdk } = useContext(SdkContext);
   return (
     <>
+      <Marquee />
       <nav className="w-full h-16 bg-zinc-800">
         <div className="container mx-auto px-4 h-full">
           <div className="flex justify-between items-center h-full">
@@ -25,12 +26,25 @@ const Navbar = () => {
             </NavLink>
             <ul className="flex gap-x-4 text-white items-center">
               <li>
-                <NavLink to={{ pathname: "/" }}>Home</NavLink>
+                <NavLink
+                  className={({ isActive }) => {
+                    if (isActive) {
+                      return "text-transparent bg-gradient-to-tr from-purple-500 to-pink-500 bg-clip-text";
+                    }
+                  }}
+                  to="/"
+                >
+                  Home
+                </NavLink>
               </li>
               <li>
                 <NavLink
+                  className={({ isActive }) => {
+                    if (isActive) {
+                      return "text-transparent bg-gradient-to-tr from-purple-500 to-pink-500 bg-clip-text";
+                    }
+                  }}
                   to={{ pathname: sdk ? "/claim" : "setup" }}
-                  className="px-2 py-2 bg-gradient-to-tr from-purple-500 to-pink-500 rounded hover:from-purple-700 hover:to-pink-700 transition"
                 >
                   Wallet
                 </NavLink>
@@ -39,7 +53,6 @@ const Navbar = () => {
           </div>
         </div>
       </nav>
-      <Marquee />
     </>
   );
 };
