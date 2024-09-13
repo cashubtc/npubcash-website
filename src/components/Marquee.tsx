2707d45a591967568d50b979810efe58fb6b7bc8
Egge <egge@getcurrent.io>
2024-09-13 07:32:55 +0200

    minimal marquee

--- src/components/Marquee.tsx
+++ src/components/Marquee.tsx
@@ -1,40 +1,11 @@
 function Marquee() {
   return (
-    <div className="bg-zinc-700 flex overflow-x-hidden relative">
-      <div className="italic animate-marquee relative whitespace-nowrap">
-        <span className="mx-8">
-          ⚠️
-          <strong>WARNING</strong>: This is a Work-In-Progress POC. Don't be
-          reckless. No warranties are made ⚠️
-        </span>
-        <span className="mx-8">
-          ⚠️
-          <strong>WARNING</strong>: This is a Work-In-Progress POC. Don't be
-          reckless. No warranties are made ⚠️
-        </span>
-        <span className="mx-8">
-          ⚠️
-          <strong>WARNING</strong>: This is a Work-In-Progress POC. Don't be
-          reckless. No warranties are made ⚠️
-        </span>
-      </div>
-      <div className="italic absolute top-0  animate-marquee2 whitespace-nowrap">
-        <span className="mx-8">
-          ⚠️
-          <strong>WARNING</strong>: This is a Work-In-Progress POC. Don't be
-          reckless. No warranties are made ⚠️
-        </span>
-        <span className="mx-8">
-          ⚠️
-          <strong>WARNING</strong>: This is a Work-In-Progress POC. Don't be
-          reckless. No warranties are made ⚠️
-        </span>
-        <span className="mx-8">
-          ⚠️
-          <strong>WARNING</strong>: This is a Work-In-Progress POC. Don't be
-          reckless. No warranties are made ⚠️
-        </span>
-      </div>
+    <div className="italic relative bg-zinc-700 text-center p-1">
+      <span>
+        ⚠️
+        <strong>WARNING</strong>: This is a Work-In-Progress. Don't be reckless.
+        No warranties are made ⚠️
+      </span>
     </div>
   );
 }
